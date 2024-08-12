f01461987e127faa01cdcf44b43fe8cc5e95c045
Olaoluwa22 <olaoluwaoni22@gmail.com>
2024-08-12 23:44:47 +0100

    Initial commit

---
+++ src/test/java/file/storage_system/StorageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package file.storage_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
